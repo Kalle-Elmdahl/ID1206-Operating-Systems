5ecec6c621d3eb265ed8eeec55bc794c19570caa
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2022-11-30 14:17:22 +0100

    Fix TLB

--- lab3/main.c
+++ lab3/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #define PAGE_ENTRIES 256
 #define BIN "BACKING_STORE.bin"
-#define TLB_MAX 3
+#define TLB_MAX 16
 
 int page_table[PAGE_ENTRIES];
 
@@ -22,15 +22,8 @@ int main(int argc, char *argv[])
 
     FILE *addresses, *memory;
     tlb_t *TLB = NULL;
-    unsigned int address, counter = 0;
-
-    TLB = add_to_tlb(TLB, 10, 20);
-    TLB = add_to_tlb(TLB, 30, 40);
-    TLB = add_to_tlb(TLB, 50, 60);
-    TLB = add_to_tlb(TLB, 13, 7);
-
-    print_tlb(TLB);
-    /* if (argc < 2 || !(addresses = fopen(argv[1], "r")))
+    unsigned int address, counter = 0, miss_counter = 0;
+    if (argc < 2 || !(addresses = fopen(argv[1], "r")))
         return 1;
 
     memory = fopen(BIN, "rb");
@@ -41,16 +34,23 @@ int main(int argc, char *argv[])
     while (fscanf(addresses, "%d,", &address) > 0)
     {
         int page_number = address >> 8;
-        frame_number = consult_tlb(TLB, page_number);
-        if (frame_number == -1)
+        int frame_number = consult_tlb(TLB, page_number);
+        if (frame_number == -1) // tlb miss
         {
-            // tlb miss
+            miss_counter++;
+
             if (page_table[page_number] == -1)
-                page_table[page_number] = counter++ << 8;
-            // add to tlb?
+            {
+                frame_number = counter++ << 8;
+                page_table[page_number] = frame_number;
+            }
+            else
+                frame_number = page_table[page_number];
+
+            TLB = add_to_tlb(TLB, page_number, frame_number);
         }
 
-        int physical = address & 0xff | page_table[page_number];
+        int physical = address & 0xff | frame_number;
         char read = 0;
 
         fseek(memory, address, SEEK_SET);
@@ -58,9 +58,11 @@ int main(int argc, char *argv[])
         printf("Virtual address: %d Physical address: %d Value: %X, %d\n", address, physical, read, read);
     }
 
+    printf("Miss rate: %u\n", miss_counter / 1000 * 100);
+
     fclose(addresses);
 
-    return 0; */
+    return 0;
 }
 
 int consult_tlb(tlb_t *TLB, int page_number)
@@ -83,18 +85,20 @@ void print_tlb(tlb_t *TLB)
     print_tlb(TLB->next);
 }
 
-int resize_tlb(tlb_t *TLB, int counter) {
-    if (TLB->next == NULL && counter == TLB_MAX) {
-        free(TLB);
-        return counter;
+void resize_tlb(tlb_t *TLB, int counter)
+{
+    if (TLB == NULL)
+        return;
+    if (counter == TLB_MAX - 1 && TLB->next != NULL)
+    {
+        free(TLB->next);
+        TLB->next = NULL;
     }
-    return resize_tlb(TLB->next, counter++);
-
+    return resize_tlb(TLB->next, ++counter);
 }
 
 tlb_t *add_to_tlb(tlb_t *TLB, int page_number, int frame_number)
 {
-
     tlb_t *new_entry = malloc(sizeof(tlb_t));
     new_entry->page_number = page_number;
     new_entry->frame_number = frame_number;
